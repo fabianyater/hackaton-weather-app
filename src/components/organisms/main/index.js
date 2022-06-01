e7b4a323f82345fce4ddcefb5cde8485adf3e229
Fabian <fabianyate31@gmail.com>
2022-06-01 16:59:56 -0500

    Implement loaders

--- src/components/organisms/main/index.js
+++ src/components/organisms/main/index.js
@@ -3,13 +3,13 @@ import React, { useState, useEffect, useContext } from 'react'
 import { getRealtimeWeather } from '../../../services/weather-api'
 import { detailsData } from './detailsData';
 import { LocationContext } from '../../../context/locationContext';
+import { testDate } from '../../../constants/formatDate';
+import { RaceBy } from '@uiball/loaders'
 
 import DetailsList from '../../molecules/details-list';
 import Temperature from '../../molecules/temperature';
 import Condition from '../../molecules/condition';
-import Shimmer from '../../atoms/shimmer';
 import './styles.css'
-import { testDate } from '../../../constants/formatDate';
 
 const Main = () => {
 
@@ -28,28 +28,37 @@ const Main = () => {
   }, [data])
 
   return data ? (
-    <main className={data.current.temp_c > 20 ? 'main hot' : 'main fresh'}>
-      <h1 className='main__title'>{data.location.name} 
-        <span className='localtime'> {testDate(data.location.localtime)}</span>
-      </h1>
-      <div className='main__wrapper'>
-        <Condition
-          source={data.current.condition.icon}
-          alt='Time condition'
-          condition={data.current.condition.text}
-        />
+    <>
+      <main className={data.current.temp_c > 20 ? 'main hot' : 'main fresh'}>
+        <h1 className='main__title'>{data.location.name}
+          <span className='localtime'> {testDate(data.location.localtime)}</span>
+        </h1>
+        <div className='main__wrapper'>
+          <Condition
+            source={data.current.condition.icon}
+            alt='Time condition'
+            condition={data.current.condition.text}
+          />
 
-        <Temperature
-          celsius={data.current.temp_c}
-          farenheit={data.current.temp_f}
-        />
+          <Temperature
+            celsius={data.current.temp_c}
+            farenheit={data.current.temp_f}
+          />
 
-        {info && <DetailsList details={info} />}
-      </div>
-    </main>
+          {info && <DetailsList details={info} />}
+        </div>
+      </main>
+    </>
   )
     :
-    (<Shimmer height={300} />)
+    (
+      <RaceBy
+        size={80}
+        lineWeight={5}
+        speed={1.4}
+        color="black"
+      />
+    )
 }
 
 export default Main
