9b5e02de2c468efdf1671adaa207dd384bcbd3d3
Fabia Yate <fabianyate31@gmail.com>
2022-06-01 19:47:19 -0500

    format dates

--- src/constants/formatDate.js
+++ src/constants/formatDate.js
@@ -1,15 +1,20 @@
-const days = ['DOMINGO', 'LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES', 'SÁBADO'];
+const days = ['LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES', 'SÁBADO', 'DOMINGO'];
 
-export const getDay = (date) => {
+export const getDay = (date, startsAtSunday) => {
   const newDate = new Date(date);
-  let day = days[newDate.getDay()];
+  let day;
+  if(startsAtSunday){
+    day = days[newDate.getDay()-1];
+  }else{
+    day = days[newDate.getDay()];
+  }
   return day;
 }
 
 export const formatDate = (date) => {
   const options = { year: 'numeric', month: 'long', day: 'numeric' };
   const newDate = new Date(date);
-  let formattedDate = newDate.toLocaleDateString('es-ES', options);
+  let formattedDate = newDate.toLocaleDateString(undefined, options);
   return formattedDate;
 }
 
